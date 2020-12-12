dea007660bb5bba00525a9d107afbb13386e724e
Anton Ždanov <azdanov@pm.me>
2020-12-12 12:37:01 +0200

    feat: create forms package

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -1,17 +1,16 @@
 package main
 
 import (
+	"azdanov.js.org/textbox/pkg/forms"
 	"azdanov.js.org/textbox/pkg/models"
 	"html/template"
-	"net/url"
 	"path/filepath"
 	"time"
 )
 
 type templateData struct {
 	CurrentYear int
-	FormData    url.Values
-	FormErrors  map[string]string
+	Form        *forms.Form
 	Snippet     *models.Snippet
 	Snippets    []*models.Snippet
 }
