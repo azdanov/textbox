6064cf8090840d3e884c038da28dbd8bde901c49
Anton Ždanov <azdanov@pm.me>
2021-01-11 17:08:19 +0200

    test: add test helpers and handlers tests

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -33,3 +33,19 @@ func TestPing(t *testing.T) {
 		t.Errorf("want body to equal %q", "OK")
 	}
 }
+
+func TestPingIT(t *testing.T) {
+	app := newTestApplication(t)
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
+
+	code, _, body := ts.get(t, "/ping")
+
+	if code != http.StatusOK {
+		t.Errorf("want %d; got %d", http.StatusOK, code)
+	}
+
+	if string(body) != "OK" {
+		t.Errorf("want body to equal %q", "OK")
+	}
+}
